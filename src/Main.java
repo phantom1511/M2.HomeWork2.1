da86a1f730aa3cb171ed72f9b0a76387578f5c4c
Dastan Tulokulov <dastan.tulokulov.kg@mail.ru>
2019-08-29 01:29:19 +0600

    hope it is correct

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import static sun.misc.Version.print;
+
 public class Main {
+
 
     public static void main(String[] args) {
         /*IronMan ironMan = new IronMan("Tony Stark", 100, "Shooting and Flying", "Iron Man suit Mark 13");
@@ -6,7 +9,7 @@ public class Main {
         Thor thor = new Thor("Thor", 500, "God of Thunder", "Sekira special ax, which can hold power of thunder");
         Hulk hulk = new Hulk("Bruce Baner", 150, "Crazy physical power", "Hulk comes out when he is angry");
 
-        ironMan.print();
+        /*ironMan.print();
         System.out.println("-----------------------------------------------");
         captainAmerica.print();
         System.out.println("-----------------------------------------------");
@@ -14,39 +17,45 @@ public class Main {
         System.out.println("-----------------------------------------------");
         hulk.print();*/
 
-        createObject("Iron Man");
+        /*createObject("Iron Man");
         System.out.println("-----------------------------------------------");
         createObject("Captain America");
         System.out.println("-----------------------------------------------");
         createObject("Thor");
         System.out.println("-----------------------------------------------");
-        createObject("Hulk");
+        createObject("Hulk");*/
+
+        ((Printable) createObject("Iron Man")).print();
+
     }
 
-    public static String createObject( String Printable) {
-        IronMan ironMan = new IronMan("Tony Stark", 100, "Shooting and Flying", "Iron Man suit Mark 13");
-        CaptainAmerica captainAmerica = new CaptainAmerica("Stive Rogers", 100, "Unreal Power for human", "Shield from Titan");
-        Thor thor = new Thor("Thor", 500, "God of Thunder", "Sekira special ax, which can hold power of thunder");
-        Hulk hulk = new Hulk("Bruce Baner", 150, "Crazy physical power", "Hulk comes out when he is angry");
+    public static String createObject(String printable) {
+        Printable ironMan = new IronMan("Tony Stark", 100, "Shooting and Flying", "Iron Man suit Mark 13");
+        Printable captainAmerica = new CaptainAmerica("Stive Rogers", 100, "Unreal Power for human", "Shield from Titan");
+        Printable thor = new Thor("Thor", 500, "God of Thunder", "Sekira special ax, which can hold power of thunder");
+        Printable hulk = new Hulk("Bruce Baner", 150, "Crazy physical power", "Hulk comes out when he is angry");
 
-        String print = Printable;
-        switch (print) {
+        switch (printable) {
             case "Iron Man":
-                ironMan.print();
+                Printable ironMan1 = new IronMan("Tony Stark", 100, "Shooting and Flying", "Iron Man suit Mark 13");
+                ironMan1 = ironMan;
                 break;
             case "Captain America":
-                captainAmerica.print();
+                Printable captainAmerica1 = new CaptainAmerica("Stive Rogers", 100, "Unreal Power for human", "Shield from Titan");
+                captainAmerica1 = captainAmerica;
                 break;
             case "Thor":
-                thor.print();
+                Printable thor1 = new Thor("Thor", 500, "God of Thunder", "Sekira special ax, which can hold power of thunder");
+                thor1 = thor;
                 break;
             case "Hulk":
-                hulk.print();
+                Printable hulk1 = new Hulk("Bruce Baner", 150, "Crazy physical power", "Hulk comes out when he is angry");
+                hulk1 = hulk;
                 break;
             default:
                 System.out.println("Enter correct type");
                 break;
         }
-        return Printable;
+        return printable;
     }
 }
